59bb1b3b2c98886dbc3b9789909446b5ac3fbbd8
AristideAbeng <abengaristide@gmail.com>
2026-02-09 06:03:19 -0500

    Revert "adding changes to be reverted"
    This reverts commit 87ea28f2a46e8f66e066acbb6adc8efa0504568d.

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -1,5 +1,4 @@
- # Here is a new update by AristideAbeng
- # Here is another update by AristideAbeng
+ # Here is a new update by AristideA
 # Shipping Cost Calculator
 
 ## Input package weight and shipping rate
